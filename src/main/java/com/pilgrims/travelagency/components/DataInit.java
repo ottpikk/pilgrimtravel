eda633887ba2f4d89bba8aeb18f42c3b3385730c
erik-vm <60032776+erik-vm@users.noreply.github.com>
2022-09-05 20:26:03 +0300

    added AuditAwareHandler, AuditConfiguration and made some modifications in DataInit

--- src/main/java/com/pilgrims/travelagency/components/DataInit.java
+++ src/main/java/com/pilgrims/travelagency/components/DataInit.java
@@ -1,6 +1,7 @@
 package com.pilgrims.travelagency.components;
 
 import com.github.javafaker.Faker;
+import com.pilgrims.travelagency.exceptions.AuthorityNotFoundException;
 import com.pilgrims.travelagency.models.Authority;
 import com.pilgrims.travelagency.models.User;
 import com.pilgrims.travelagency.services.AuthorityService;
@@ -11,8 +12,7 @@ import org.springframework.stereotype.Component;
 import javax.annotation.PostConstruct;
 import java.time.ZoneId;
 
-import static com.pilgrims.travelagency.utils.Constants.Security.AUTHORITY_ADMIN;
-import static com.pilgrims.travelagency.utils.Constants.Security.AUTHORITY_CUSTOMER;
+import static com.pilgrims.travelagency.utils.Constants.Security.*;
 
 /**
  * Component to initialize data on app startup
@@ -31,45 +31,43 @@ public class DataInit {
     @Autowired
     private AuthorityService authorityService;
 
+   private Faker faker = new Faker();
 
     @PostConstruct
-    public void init() {
-        //initAuthorityData();
-           initUserData();
-        //populateDBWithUsers();
+    public void init() throws AuthorityNotFoundException {
+        initAuthorityData();
+        initUserData();
+        populateDBWithUsers();
     }
 
     // PRIVATE METHODS //
 
-    /**
-     * private void initAuthorityData() {
-     * System.out.println("Starting initializing Authority..");
-     * Authority authorityAdmin = new Authority();
-     * authorityAdmin.setName(AUTHORITY_ADMIN);
-     * createAuthority(authorityAdmin);
-     * <p>
-     * Authority authorityCustomer = new Authority();
-     * authorityCustomer.setName(AUTHORITY_CUSTOMER);
-     * createAuthority(authorityCustomer);
-     * <p>
-     * Authority authorityGuest = new Authority();
-     * authorityGuest.setName(AUTHORITY_GUEST);
-     * createAuthority(authorityGuest);
-     * }
-     */
 
-    private void initUserData() {
+      private void initAuthorityData() {
+      System.out.println("Starting initializing Authority..");
+      Authority authorityAdmin = new Authority();
+      authorityAdmin.setName(AUTHORITY_ADMIN);
+          authorityService.createAuthority(authorityAdmin);
+
+      Authority authorityCustomer = new Authority();
+      authorityCustomer.setName(AUTHORITY_CUSTOMER);
+      authorityService.createAuthority(authorityCustomer);
+
+      Authority authorityGuest = new Authority();
+      authorityGuest.setName(AUTHORITY_GUEST);
+      authorityService.createAuthority(authorityGuest);
+      }
+
+
+    private void initUserData() throws AuthorityNotFoundException {
         System.out.println("Starting initializing User..");
-        Authority authority = new Authority();
-        authority.setName(AUTHORITY_ADMIN);
-        authorityService.createAuthority(authority);
         User user = new User();
         user.setUserName("test");
         user.setPassword("test");
         user.setActive(true);
         user.setAddress("Linn 1, Tänav 2, maja ja korter");
         user.setEmail("suvaline@domeen.com");
-        user.setAuthority(authority);
+        user.setAuthority(authorityService.findAuthorityByName(AUTHORITY_ADMIN));
 
         try {
             userService.createUser(user);
@@ -91,11 +89,9 @@ public class DataInit {
      * }
      */
 
-    private void populateDBWithUsers() {
-        Faker faker = new Faker();
-          Authority authority = new Authority();
-          authority.setName(AUTHORITY_CUSTOMER);
-          authorityService.createAuthority(authority);
+    private void populateDBWithUsers() throws AuthorityNotFoundException {
+
+
         for (int i = 0; i < 30; i++) {
             User user = new User();
             String firstName = faker.name().firstName();
@@ -107,8 +103,8 @@ public class DataInit {
             user.setUserName(email);
             user.setPhoneNumber(faker.phoneNumber().toString());
             user.setActive(true);
-            user.setAddress(faker.address().streetAddress() + ", " + faker.address().streetAddressNumber() + ", " + faker.address().cityName());
-            user.setAuthority(authority);
+            user.setAddress(faker.address().streetAddress()  + ", " + faker.address().cityName());
+            user.setAuthority(authorityService.findAuthorityByName(AUTHORITY_CUSTOMER));
             user.setPassword("password");
             user.setDateOfBirth(faker.date().birthday().toInstant().atZone(ZoneId.systemDefault()).toLocalDate());
             userService.createUser(user);
