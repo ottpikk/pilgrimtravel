ed55adbcccc7956131c16ea75a7d518f44c852a7
erik-vm <60032776+erik-vm@users.noreply.github.com>
2022-09-05 19:12:30 +0300

    added faker dependency and created method inside DataInit to prepopulate database with users, using faker

--- src/main/java/com/pilgrims/travelagency/components/DataInit.java
+++ src/main/java/com/pilgrims/travelagency/components/DataInit.java
@@ -1,7 +1,6 @@
 package com.pilgrims.travelagency.components;
 
-import com.pilgrims.travelagency.exceptions.AuthorityNotFoundException;
-import com.pilgrims.travelagency.exceptions.UserNotFoundException;
+import com.github.javafaker.Faker;
 import com.pilgrims.travelagency.models.Authority;
 import com.pilgrims.travelagency.models.User;
 import com.pilgrims.travelagency.services.AuthorityService;
@@ -10,10 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import java.time.ZoneId;
 
-import java.sql.Date;
-
-import static com.pilgrims.travelagency.utils.Constants.Security.*;
+import static com.pilgrims.travelagency.utils.Constants.Security.AUTHORITY_ADMIN;
 
 /**
  * Component to initialize data on app startup
@@ -24,8 +22,7 @@ import static com.pilgrims.travelagency.utils.Constants.Security.*;
 public class DataInit {
 
     /**
-    @Autowired
-    private AuthorityService authorityService;
+     * @Autowired private AuthorityService authorityService;
      */
 
     @Autowired
@@ -35,31 +32,32 @@ public class DataInit {
 
 
     @PostConstruct
-    public void init() throws AuthorityNotFoundException {
+    public void init() {
         //initAuthorityData();
-        initUserData();
+        //   initUserData();
+        populateDBWithUsers();
     }
 
     // PRIVATE METHODS //
 
     /**
-    private void initAuthorityData() {
-        System.out.println("Starting initializing Authority..");
-        Authority authorityAdmin = new Authority();
-        authorityAdmin.setName(AUTHORITY_ADMIN);
-        createAuthority(authorityAdmin);
-
-        Authority authorityCustomer = new Authority();
-        authorityCustomer.setName(AUTHORITY_CUSTOMER);
-        createAuthority(authorityCustomer);
-
-        Authority authorityGuest = new Authority();
-        authorityGuest.setName(AUTHORITY_GUEST);
-        createAuthority(authorityGuest);
-    }
+     * private void initAuthorityData() {
+     * System.out.println("Starting initializing Authority..");
+     * Authority authorityAdmin = new Authority();
+     * authorityAdmin.setName(AUTHORITY_ADMIN);
+     * createAuthority(authorityAdmin);
+     * <p>
+     * Authority authorityCustomer = new Authority();
+     * authorityCustomer.setName(AUTHORITY_CUSTOMER);
+     * createAuthority(authorityCustomer);
+     * <p>
+     * Authority authorityGuest = new Authority();
+     * authorityGuest.setName(AUTHORITY_GUEST);
+     * createAuthority(authorityGuest);
+     * }
      */
 
-    private void initUserData() throws AuthorityNotFoundException {
+    private void initUserData() {
         System.out.println("Starting initializing User..");
         Authority authority = new Authority();
         authority.setName(AUTHORITY_ADMIN);
@@ -73,20 +71,44 @@ public class DataInit {
 
         try {
             userService.createUser(user);
-            System.out.println("User " +user.getUserName()+" created successfully");
+            System.out.println("User " + user.getUserName() + " created successfully");
 
         } catch (Exception e) {
             System.out.println(e.getLocalizedMessage());
         }
     }
 
-    /**private void createAuthority(Authority authority) {
-        try {
-            Authority resultAuthority = authorityService.findAuthorityByName(authority.getName());
-            System.out.println("Cannot pre-initialize authority:" + resultAuthority.getName());
-        } catch (AuthorityNotFoundException authorityNotFoundException) {
-            authorityService.createAuthority(authority);
+    /**
+     * private void createAuthority(Authority authority) {
+     * try {
+     * Authority resultAuthority = authorityService.findAuthorityByName(authority.getName());
+     * System.out.println("Cannot pre-initialize authority:" + resultAuthority.getName());
+     * } catch (AuthorityNotFoundException authorityNotFoundException) {
+     * authorityService.createAuthority(authority);
+     * }
+     * }
+     */
+
+    private void populateDBWithUsers() {
+        Faker faker = new Faker();
+        //   Authority authority = new Authority();
+        //  authority.setName(AUTHORITY_CUSTOMER);
+        for (int i = 0; i < 30; i++) {
+            User user = new User();
+            String firstName = faker.name().firstName();
+            String lastName = faker.name().lastName();
+            String email = String.format("%s.%s@sda.com", firstName, lastName);
+            user.setFirstName(firstName);
+            user.setLastName(lastName);
+            user.setEmail(email);
+            user.setUserName(email);
+            user.setPhoneNumber(faker.phoneNumber().toString());
+            user.setActive(true);
+            user.setAddress(faker.address().streetAddress() + ", " + faker.address().streetAddressNumber() + ", " + faker.address().cityName());
+            // user.setAuthority(authority);
+            user.setPassword("password");
+            user.setDateOfBirth(faker.date().birthday().toInstant().atZone(ZoneId.systemDefault()).toLocalDate());
+            userService.createUser(user);
         }
     }
-     */
 }
